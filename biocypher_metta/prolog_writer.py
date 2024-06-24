8497e78c824d3591fb2f556f010859ec932aa013
Abdulrahman S. Omar <hsamireh@gmail.com>
2024-06-24 15:16:01 +0300

    lower() method should only be called on string properties

--- biocypher_metta/prolog_writer.py
+++ biocypher_metta/prolog_writer.py
@@ -126,9 +126,9 @@ class PrologWriter:
 
             special_chars = ["(", ")"]
             escape_char = "\\"
-            return "".join(escape_char + c if c in special_chars or c == escape_char else c for c in prop)
+            return "".join(escape_char + c if c in special_chars or c == escape_char else c for c in prop).lower()
 
-        return prop.lower()
+        return prop
 
     def convert_input_labels(self, label, replace_char="_"):
         """
