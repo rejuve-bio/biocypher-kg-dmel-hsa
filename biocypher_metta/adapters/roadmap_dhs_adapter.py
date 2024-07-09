cf372f325a49ff9762ce7d60a846363faa4b97a7
Abdulrahman S. Omar <hsamireh@gmail.com>
2024-07-09 18:17:12 +0300

    Split the roadmap adapter into three adapters for each datatype

---
+++ biocypher_metta/adapters/roadmap_dhs_adapter.py
@@ -0,0 +1,70 @@
+# Author Abdulrahman S. Omar <xabush@singularitynet.io>
+import csv
+import gzip
+import os.path
+import pickle
+from biocypher_metta.adapters import Adapter
+from biocypher_metta.adapters.helpers import check_genomic_location
+
+# Example roadmap csv input files
+# rsid,dataset,cell,tissue,datatype
+# rs10,erc2-DHS,"E050 Primary hematopoietic stem cells G-CSF-mobili",Blood,"DNase I Hotspot"
+# rs10,erc2-DHS,"E028 Breast variant Human Mammary Epithelial Cells",Breast,"DNase I Hotspot"
+# rs10000009,erc2-DHS,"E094 Gastric",Gastric,"DNase I Hotspot"
+# rs1000001,erc2-DHS,"E090 Fetal Muscle Leg","Fetal Muscle Leg","DNase I Hotspot"
+
+COL_DICT = {'rsid': 0, 'dataset': 1, 'cell': 2, 'tissue': 3, 'datatype': 4}
+
+class RoadMapDHSAdapter(Adapter):
+    def __init__(self, filepath, tissue_to_ontology_id_map, 
+                 dbsnp_rsid_map, write_properties, add_provenance,
+                 chr=None, start=None, end=None):
+        """
+        :param filepath: path to the directory containing epigenomic data
+        :param dbsnp_rsid_map: a dictionary mapping dbSNP rsid to genomic position
+        :param chr: chromosome name
+        :param start: start position
+        :param end: end position
+        """
+        self.filepath = filepath
+        self.tissue_to_ontology_id_map = pickle.load(open(tissue_to_ontology_id_map, 'rb'))
+        self.dbsnp_rsid_map = dbsnp_rsid_map
+        self.chr = chr
+        self.start = start
+        self.end = end
+
+        self.source = "Roadmap Epigenomics Project"
+        self.source_url = "https://forgedb.cancer.gov/api/forge2.erc2-DHS/v1.0/forge2.erc2-DHS.forgedb.csv.gz" # {0-9} indicates this dataset is split into 10 parts
+        self.label = "in_dnase_I_hotspot"
+
+        super(RoadMapAdapter, self).__init__(write_properties, add_provenance)
+
+    def get_edges(self):
+        with gzip.open(self.filepath, "rt") as fp:
+            next(fp)
+            reader = csv.reader(fp, delimiter=',')
+            for row in reader:
+                try:
+                    _id = row[0]
+                    chr = self.dbsnp_rsid_map[_id]["chr"]
+                    pos = self.dbsnp_rsid_map[_id]["pos"]
+                    tissue = row[COL_DICT['tissue']].replace('"', '').replace("'", '')
+                    biological_context = self.tissue_to_ontology_id_map.get(tissue, None) # TODO use cell type
+                    if check_genomic_location(self.chr, self.start, self.end, chr, pos, pos):
+                        _props = {}
+                        if biological_context == None:
+                            print(f"{tissue} not found in ontology map skipping...")
+                            continue
+                        
+                        _source = _id,
+                        _target = biological_context
+
+                        if self.write_properties and self.add_provenance:
+                            _props['source'] = self.source
+                            _props['source_url'] = self.source_url
+                                
+                        yield _source, _target, self.label, _props
+
+                except Exception as e:
+                    print(f"error while parsing row: {row}, error: {e} skipping...")
+                    continue
--- biocypher_metta/adapters/roadmap_state_adapter.py
+++ biocypher_metta/adapters/roadmap_state_adapter.py
@@ -5,16 +5,16 @@ import os.path
 import pickle
 from biocypher_metta.adapters import Adapter
 from biocypher_metta.adapters.helpers import check_genomic_location
+
 # Example roadmap csv input files
 # rsid,dataset,cell,tissue,datatype
-# rs10,erc2-DHS,"E050 Primary hematopoietic stem cells G-CSF-mobili",Blood,"DNase I Hotspot"
-# rs10,erc2-DHS,"E028 Breast variant Human Mammary Epithelial Cells",Breast,"DNase I Hotspot"
-# rs10000009,erc2-DHS,"E094 Gastric",Gastric,"DNase I Hotspot"
-# rs1000001,erc2-DHS,"E090 Fetal Muscle Leg","Fetal Muscle Leg","DNase I Hotspot"
+# rs10000007,erc2-chromatin15state-all,E063 Adipose Nuclei,Adipose,TxWk
+# rs10000007,erc2-chromatin15state-all,E080 Fetal Adrenal Gland,Adrenal,Quies
+# rs10000007,erc2-chromatin15state-all,E029 Primary monocytes from peripheral blood,Blood,Quies
 
+COL_DICT = {'rsid': 0, 'dataset': 1, 'cell': 2, 'tissue': 3, 'datatype': 4}
 
-class RoadMapAdapter(Adapter):
-    INDEX = {'rsid': 0, 'dataset': 1, 'cell': 2, 'tissue': 3, 'datatype': 4}
+class RoadMapChromatinStateAdapter(Adapter):
     def __init__(self, filepath, tissue_to_ontology_id_map, 
                  dbsnp_rsid_map, write_properties, add_provenance,
                  chr=None, start=None, end=None):
@@ -26,27 +26,20 @@ class RoadMapAdapter(Adapter):
         :param end: end position
         """
         self.filepath = filepath
+        assert os.path.isdir(self.filepath), "The path to the directory containing epigenomic data is not directory"
         self.tissue_to_ontology_id_map = pickle.load(open(tissue_to_ontology_id_map, 'rb'))
         self.dbsnp_rsid_map = dbsnp_rsid_map
         self.chr = chr
         self.start = start
         self.end = end
-        assert os.path.isdir(self.filepath), "The path to the directory containing epigenomic data is not directory"
 
-        self.source = 'Roadmap Epigenomics Project'
-        self.source_url = ['https://forgedb.cancer.gov/api/forge2.erc2-chromatin15state-all/v1.0/forge2.erc2'
-                           '-chromatin15state-all.{0-9}.forgedb.csv.gz',
-                           "https://forgedb.cancer.gov/api/forge2.erc2-H3-all/v1.0/forge2.erc2-H3-all.{"
-                           "0-9}.forgedb.csv.gz",
-                           "https://forgedb.cancer.gov/api/forge2.erc2-DHS/v1.0/forge2.erc2-DHS.forgedb.csv.gz"] # {0-9} indicates this dataset is split into
-        # 10 parts
-        self.label = "regulatory_region"
+        self.source = "Roadmap Epigenomics Project"
+        self.source_url = "https://forgedb.cancer.gov/api/forge2.erc2-chromatin15state-all/v1.0/forge2.erc2-chromatin15state-all.{0-9}.forgedb.csv.gz" # {0-9} indicates this dataset is split into 10 parts
+        self.label = "chromatin_state"
 
         super(RoadMapAdapter, self).__init__(write_properties, add_provenance)
 
-
-    def get_nodes(self):
-
+    def get_edges(self):
         for file_name in os.listdir(self.filepath):
             with gzip.open(os.path.join(self.filepath, file_name), "rt") as fp:
                 next(fp)
@@ -56,25 +49,23 @@ class RoadMapAdapter(Adapter):
                         _id = row[0]
                         chr = self.dbsnp_rsid_map[_id]["chr"]
                         pos = self.dbsnp_rsid_map[_id]["pos"]
-                        tissue = row[RoadMapAdapter.INDEX['tissue']].replace('"', '').replace("'", '')
-                        biological_context = self.tissue_to_ontology_id_map.get(tissue, None)
+                        tissue = row[COL_DICT['tissue']].replace('"', '').replace("'", '')
+                        biological_context = self.tissue_to_ontology_id_map.get(tissue, None) # TODO use cell type
                         if check_genomic_location(self.chr, self.start, self.end, chr, pos, pos):
                             _props = {}
                             if biological_context == None:
                                 print(f"{tissue} not found in ontology map skipping...")
                                 continue
-
+                            
+                            _source = _id,
+                            _target = biological_context
                             if self.write_properties:
-                                _props = {
-                                    'cell': row[RoadMapAdapter.INDEX['cell']],
-                                    'biological_context': biological_context,
-                                    'biochemical_activity': row[RoadMapAdapter.INDEX['datatype']]
-                                }
+                                _props["state"] = row[COL_DICT['datatype']]
                                 if self.add_provenance:
                                     _props['source'] = self.source
                                     _props['source_url'] = self.source_url
                                     
-                            yield _id, self.label, _props
+                            yield _source, _target, self.label, _props
 
                     except Exception as e:
                         print(f"error while parsing row: {row}, error: {e} skipping...")
