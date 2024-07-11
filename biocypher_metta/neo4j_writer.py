d49330d579c4c862ed047bef16fbd85bfb66b639
Basliel-Amsalu <acychappiekiller@gmail.com>
2024-07-11 10:16:28 +0300

    write nodes

--- biocypher_metta/neo4j_writer.py
+++ biocypher_metta/neo4j_writer.py
@@ -23,7 +23,7 @@ class Neo4jWriter:
         self.create_edge_types()
 
         self.excluded_properties = []
-        
+
     def create_edge_types(self):
             schema = self.bcy._get_ontology_mapping()._extend_schema()
             self.edge_node_types = {}
@@ -55,3 +55,65 @@ class Neo4jWriter:
                                 output_label.lower() if output_label is not None else None
                             ),
                         }
+    def write_nodes(self, nodes, path_prefix=None, create_dir=True):
+        if path_prefix is not None:
+            file_path = f"{self.output_path}/{path_prefix}/nodes.cypher"
+            if create_dir:
+                if not os.path.exists(f"{self.output_path}/{path_prefix}"):
+                    pathlib.Path(f"{self.output_path}/{path_prefix}").mkdir(
+                        parents=True, exist_ok=True
+                    )
+        else:
+            file_path = f"{self.output_path}/nodes.cypher"
+
+        with open(file_path, "a") as f:
+            for node in nodes:
+                query = self.write_node(node)
+                f.write(query + "\n")
+
+        logger.info("Finished writing out nodes")
+    
+    def write_node(self, node):
+        id, label, properties = node
+        if "." in label:
+            label = label.split(".")[1]
+        label = label.lower()
+        id = id.lower()
+        properties_str = self._format_properties(properties)
+        return f"CREATE (:{label} {{id: '{id}',{properties_str}}})"
+    
+    def _format_properties(self, properties):
+        """
+        Format properties into a Cypher string.
+        :param properties: Dictionary of properties
+        :return: Cypher formatted properties string
+        """
+        out_str = []
+        for k, v in properties.items():
+            if k in self.excluded_properties or v is None or v == "":
+                continue
+            if isinstance(v, list):
+                prop = "[" + ", ".join(f"'{e}'" for e in v) + "]"
+            elif isinstance(v, dict):
+                prop = self._format_properties(v)
+            else:
+                prop = f"'{v}'"
+            out_str.append(f"{k}: {prop}")
+        return ", ".join(out_str)
+
+    def convert_input_labels(self, label, replace_char="_"):
+        """
+        A method that removes spaces in input labels and replaces them with replace_char
+        :param label: Input label of a node or edge
+        :param replace_char: the character to replace spaces with
+        :return:
+        """
+        return label.replace(" ", replace_char)
+
+    def get_parent(self, G, node):
+        """
+        Get the immediate parent of a node in the ontology.
+        """
+        return nx.dfs_preorder_nodes(G, node, depth_limit=2)
+
+
