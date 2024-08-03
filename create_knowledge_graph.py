d94ce4779377a804e6baaa26c19357ea6b720474
Basliel-Amsalu <acychappiekiller@gmail.com>
2024-08-03 13:14:54 +0300

    get writter for neo4j

--- create_knowledge_graph.py
+++ create_knowledge_graph.py
@@ -1,24 +1,50 @@
 """
 Knowledge graph generation through BioCypher script
 """
+
 from biocypher_metta.metta_writer import *
+from biocypher_metta.neo4j_writer import *
 from biocypher._logger import logger
 import typer
 import yaml
-import importlib #for reflection
+import importlib  # for reflection
 from typing_extensions import Annotated
 import pickle
 
 app = typer.Typer()
 
+
+def get_writer(writer_type: str, output_dir: pathlib.Path):
+    if writer_type == "neo4j":
+        return Neo4jWriter(
+            schema_config="config/schema_config.yaml",
+            biocypher_config="config/biocypher_config.yaml",
+            output_dir=output_dir,
+        )
+    else:
+        raise ValueError(f"Unknown writer type: {writer_type}")
+
+
 # Run build
 @app.command()
-def main(output_dir: Annotated[pathlib.Path, typer.Option(exists=True, file_okay=False, dir_okay=True)],
-         adapters_config: Annotated[pathlib.Path, typer.Option(exists=True, file_okay=True, dir_okay=False)],
-         dbsnp_rsids: Annotated[pathlib.Path, typer.Option(exists=True, file_okay=True, dir_okay=False)],
-         dbsnp_pos: Annotated[pathlib.Path, typer.Option(exists=True, file_okay=True, dir_okay=False)],
-         write_properties: bool = typer.Option(True, help="Write properties to nodes and edges"),
-         add_provenance: bool = typer.Option(True, help="Add provenance to nodes and edges")):
+def main(
+    output_dir: Annotated[
+        pathlib.Path, typer.Option(exists=True, file_okay=False, dir_okay=True)
+    ],
+    adapters_config: Annotated[
+        pathlib.Path, typer.Option(exists=True, file_okay=True, dir_okay=False)
+    ],
+    dbsnp_rsids: Annotated[
+        pathlib.Path, typer.Option(exists=True, file_okay=True, dir_okay=False)
+    ],
+    dbsnp_pos: Annotated[
+        pathlib.Path, typer.Option(exists=True, file_okay=True, dir_okay=False)
+    ],
+    write_properties: bool = typer.Option(
+        True, help="Write properties to nodes and edges"
+    ),
+    add_provenance: bool = typer.Option(True, help="Add provenance to nodes and edges"),
+):
     """
     Main function. Call individual adapters to download and process data. Build
     via BioCypher from node and edge data.
@@ -26,14 +52,15 @@ def main(output_dir: Annotated[pathlib.Path, typer.Option(exists=True, file_okay
 
     # Start biocypher
     logger.info("Loading dbsnp rsids map")
-    dbsnp_rsids_dict = pickle.load(open(dbsnp_rsids, 'rb'))
+    dbsnp_rsids_dict = pickle.load(open(dbsnp_rsids, "rb"))
     logger.info("Loading dbsnp pos map")
-    dbsnp_pos_dict = pickle.load(open(dbsnp_pos, 'rb'))
+    dbsnp_pos_dict = pickle.load(open(dbsnp_pos, "rb"))
 
-
-    bc = MeTTaWriter(schema_config="config/schema_config.yaml",
-                     biocypher_config="config/biocypher_config.yaml",
-                     output_dir=output_dir)
+    bc = Neo4jWriter(
+        schema_config="config/schema_config.yaml",
+        biocypher_config="config/biocypher_config.yaml",
+        output_dir=output_dir,
+    )
 
     # bc.show_ontology_structure()
 
@@ -52,7 +79,9 @@ def main(output_dir: Annotated[pathlib.Path, typer.Option(exists=True, file_okay
         adapter_module = importlib.import_module(adapter_config["module"])
         adapter_cls = getattr(adapter_module, adapter_config["cls"])
         ctr_args = adapter_config["args"]
-        if "dbsnp_rsid_map" in ctr_args: #this for dbs that use grch37 assembly and to map grch37 to grch38
+        if (
+            "dbsnp_rsid_map" in ctr_args
+        ):  # this for dbs that use grch37 assembly and to map grch37 to grch38
             ctr_args["dbsnp_rsid_map"] = dbsnp_rsids_dict
         if "dbsnp_pos_map" in ctr_args:
             ctr_args["dbsnp_pos_map"] = dbsnp_pos_dict
@@ -71,8 +100,8 @@ def main(output_dir: Annotated[pathlib.Path, typer.Option(exists=True, file_okay
             edges = adapter.get_edges()
             bc.write_edges(edges, path_prefix=outdir)
 
-
     logger.info("Done")
+
 
 if __name__ == "__main__":
     app()
