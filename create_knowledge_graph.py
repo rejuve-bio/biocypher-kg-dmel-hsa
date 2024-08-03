72e23a5f6b246a96fa8a655ac428bf82736b4997
Basliel-Amsalu <acychappiekiller@gmail.com>
2024-08-03 13:31:29 +0300

    wip

--- create_knowledge_graph.py
+++ create_knowledge_graph.py
@@ -1,67 +1,39 @@
 """
 Knowledge graph generation through BioCypher script
 """
-
 from biocypher_metta.metta_writer import *
 from biocypher_metta.prolog_writer import PrologWriter
-from biocypher_metta.neo4j_writer import *
 from biocypher._logger import logger
 import typer
 import yaml
-import importlib  # for reflection
+import importlib #for reflection
 from typing_extensions import Annotated
 import pickle
 
 app = typer.Typer()
 
-
 # Function to choose the writer class based on user input
 def get_writer(writer_type: str, output_dir: pathlib.Path):
-    if writer_type == "metta":
-        return MeTTaWriter(
-            schema_config="config/schema_config.yaml",
-            biocypher_config="config/biocypher_config.yaml",
-            output_dir=output_dir,
-        )
-    elif writer_type == "prolog":
-        return PrologWriter(
-            schema_config="config/schema_config.yaml",
-            biocypher_config="config/biocypher_config.yaml",
-            output_dir=output_dir,
-        )
-    elif writer_type == "neo4j":
-        return Neo4jWriter(
-            schema_config="config/schema_config.yaml",
-            biocypher_config="config/biocypher_config.yaml",
-            output_dir=output_dir,
-        )
+    if writer_type == 'metta':
+        return MeTTaWriter(schema_config="config/schema_config.yaml",
+                           biocypher_config="config/biocypher_config.yaml",
+                           output_dir=output_dir)
+    elif writer_type == 'prolog':
+        return PrologWriter(schema_config="config/schema_config.yaml",
+                            biocypher_config="config/biocypher_config.yaml",
+                            output_dir=output_dir)
     else:
         raise ValueError(f"Unknown writer type: {writer_type}")
 
-
 # Run build
 @app.command()
-def main(
-    output_dir: Annotated[
-        pathlib.Path, typer.Option(exists=True, file_okay=False, dir_okay=True)
-    ],
-    adapters_config: Annotated[
-        pathlib.Path, typer.Option(exists=True, file_okay=True, dir_okay=False)
-    ],
-    dbsnp_rsids: Annotated[
-        pathlib.Path, typer.Option(exists=True, file_okay=True, dir_okay=False)
-    ],
-    dbsnp_pos: Annotated[
-        pathlib.Path, typer.Option(exists=True, file_okay=True, dir_okay=False)
-    ],
-    writer_type: str = typer.Option(
-        default="metta", help="Choose writer type: metta, prolog"
-    ),
-    write_properties: bool = typer.Option(
-        True, help="Write properties to nodes and edges"
-    ),
-    add_provenance: bool = typer.Option(True, help="Add provenance to nodes and edges"),
-):
+def main(output_dir: Annotated[pathlib.Path, typer.Option(exists=True, file_okay=False, dir_okay=True)],
+         adapters_config: Annotated[pathlib.Path, typer.Option(exists=True, file_okay=True, dir_okay=False)],
+         dbsnp_rsids: Annotated[pathlib.Path, typer.Option(exists=True, file_okay=True, dir_okay=False)],
+         dbsnp_pos: Annotated[pathlib.Path, typer.Option(exists=True, file_okay=True, dir_okay=False)],
+         writer_type: str = typer.Option(default="metta", help="Choose writer type: metta, prolog"),
+         write_properties: bool = typer.Option(True, help="Write properties to nodes and edges"),
+         add_provenance: bool = typer.Option(True, help="Add provenance to nodes and edges")):
     """
     Main function. Call individual adapters to download and process data. Build
     via BioCypher from node and edge data.
@@ -69,9 +41,9 @@ def main(
 
     # Start biocypher
     logger.info("Loading dbsnp rsids map")
-    dbsnp_rsids_dict = pickle.load(open(dbsnp_rsids, "rb"))
+    dbsnp_rsids_dict = pickle.load(open(dbsnp_rsids, 'rb'))
     logger.info("Loading dbsnp pos map")
-    dbsnp_pos_dict = pickle.load(open(dbsnp_pos, "rb"))
+    dbsnp_pos_dict = pickle.load(open(dbsnp_pos, 'rb'))
 
     # Choose the writer based on user input or default to 'metta'
     bc = get_writer(writer_type, output_dir)
@@ -95,9 +67,7 @@ def main(
         adapter_module = importlib.import_module(adapter_config["module"])
         adapter_cls = getattr(adapter_module, adapter_config["cls"])
         ctr_args = adapter_config["args"]
-        if (
-            "dbsnp_rsid_map" in ctr_args
-        ):  # this for dbs that use grch37 assembly and to map grch37 to grch38
+        if "dbsnp_rsid_map" in ctr_args: #this for dbs that use grch37 assembly and to map grch37 to grch38
             ctr_args["dbsnp_rsid_map"] = dbsnp_rsids_dict
         if "dbsnp_pos_map" in ctr_args:
             ctr_args["dbsnp_pos_map"] = dbsnp_pos_dict
@@ -116,8 +86,8 @@ def main(
             edges = adapter.get_edges()
             bc.write_edges(edges, path_prefix=outdir)
 
-    logger.info("Done")
 
+    logger.info("Done")
 
 if __name__ == "__main__":
     app()
