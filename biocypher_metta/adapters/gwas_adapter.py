ca8afea9c54f7b81795f619c13a1ceab96103616
Basliel-Amsalu <acychappiekiller@gmail.com>
2024-08-16 17:39:41 +0300

    gwas adapter

---
+++ biocypher_metta/adapters/gwas_adapter.py
@@ -0,0 +1,76 @@
+import csv
+import os
+import pickle
+from biocypher_metta.adapters import Adapter
+from biocypher_metta.adapters.helpers import to_float, check_genomic_location
+from biocypher._logger import logger
+import gzip
+
+
+index = {
+    "rsid": 21,
+    "in_gene": 17,
+    "upstream_gene": 15,
+    "downstream_gene": 16,
+    "p_value": 27,
+    "chr": 11,
+    "pos": 12,
+}
+
+ALLOWED_LABELS = ["upstream_gene", "downstream_gene", "in_gene"]
+
+class GWASAdapter(Adapter):
+
+    def __init__(
+        self,
+        filepath,
+        write_properties,
+        add_provenance,
+        label,
+        chr=None,
+        start=None,
+        end=None,
+    ):
+        self.filepath = filepath
+        self.chr = chr
+        self.start = start
+        self.end = end
+        self.label = label
+        self.source = "GWAS"
+        self.source_url = "https://ftp.ebi.ac.uk/pub/databases/gwas/releases/2024/07/29/gwas-catalog-associations_ontology-annotated.tsv"
+
+        super(GWASAdapter, self).__init__(write_properties, add_provenance)
+
+    def get_edges(self):
+        with gzip.open(self.filepath, "rt") as gwas:
+            next(gwas)  # skip header
+            gwas_row = csv.reader(gwas)
+            for row in gwas_row:
+                try:
+                    chr, pos = row[index["chr"]], row[index["pos"]]
+                    if pos is not None:
+                        pos = int(pos)
+                    else:
+                        continue
+                    variant_id = row[index["rsid"]]
+                    if not row[index[self.label]]:
+                        continue 
+                    gene_id = row[index[self.label]]
+                    if check_genomic_location(
+                        self.chr, self.start, self.end, chr, pos, pos
+                    ):
+                        _source = variant_id
+                        _target = gene_id
+                        _props = {}
+                        if self.write_properties:
+                            _props = {
+                                "p_value": to_float(row[index["p_value"]]),
+                            }
+                            if self.add_provenance:
+                                _props["source"] = self.source
+                                _props["source_url"] = self.source_url
+
+                        yield _source, _target, self.label, _props
+                except Exception as e:
+                    print(row)
+                    print(e)
